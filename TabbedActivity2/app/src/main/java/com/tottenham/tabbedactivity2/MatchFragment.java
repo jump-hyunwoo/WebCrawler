b1f13af60051f096c6ddea154e581acd4183f78e
Hyunwoo Jeong <fantastic1156@gmail.com>
2019-11-04 09:05:21 +0900

    This is web crawler

---
+++ TabbedActivity2/app/src/main/java/com/tottenham/tabbedactivity2/MatchFragment.java
@@ -0,0 +1,158 @@
+package com.tottenham.tabbedactivity2;
+
+
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MatchFragment extends Fragment {
+
+
+    public MatchFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_match, container, false);
+        new MatchScheduler().execute();
+        return rootView;
+    }
+
+    private class MatchScheduler extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog progressDialog;
+        private String[] firstGameStr = {"","","","",""}; //date, team, stadium, time, (---)
+        private String[] secondGameStr = {"","","","",""};
+        private String test;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setProgressStyle(progressDialog.STYLE_SPINNER);
+            progressDialog.setMessage("경기 일정을 가져오고 있습니다.");
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+
+                /*
+                Document 클래스는 연결해서 얻어온 HTML 전체 문서
+                Element 클래스는 Document의 HTML 요소
+                Elements 클래스는 Element가 모인 자료형
+                */
+
+                Document doc;
+                doc = Jsoup.connect("https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" +
+                        "%ED%86%A0%ED%8A%B8%EB%84%98+%EA%B2%BD%EA%B8%B0%EC%9D%BC%EC%A0%95").get();
+
+                Elements selected = doc.select("tr[class=_selected]").select("th[colspan=7]"); //_selected 클래스는 오늘 경기를 가리켜준다.
+                Elements dates = doc.select("th[colspan=7]");
+                Elements leftTeams = doc.select("td[class=l_team]");
+                Elements rightTeams = doc.select("td[class=r_team]");
+                Elements scores = doc.select("td[class=score]");
+                Elements stadiums = doc.select("span[class=stadium_ellipsis]");
+                Elements times = doc.select("span[class=bg_none]");
+
+                String selectedDate = selected.text();//selected를 통해 해당 날짜의 인덱스 값을 구한다.
+                int index = 0;
+                for(Element date : dates) {
+                    if(selectedDate.equals(date.text())) break;
+                    index++;
+                }
+
+                firstGameStr[0] = dates.get(index).text();//구한 인덱스로 Elements에 접근한 후 원하는 데이터만 연결해준다.
+                firstGameStr[1] = leftTeams.get(index).text() + " " + scores.get(index).text() + " " + rightTeams.get(index).text();
+                firstGameStr[2] = stadiums.get(index).text();
+                firstGameStr[3] = times.get(index).text();
+
+                if(index+1 != dates.size()) {
+                    secondGameStr[0] = dates.get(index+1).text();
+                    secondGameStr[1] = leftTeams.get(index+1).text() + " " + scores.get(index+1).text() + " " + rightTeams.get(index+1).text();
+                    secondGameStr[2] = stadiums.get(index+1).text();
+                    secondGameStr[3] = times.get(index+1).text();
+                }
+
+                else if(index+1 == dates.size()) {//selected가 달의 마지막 경기일 경우
+                    String newUrl = doc.select("span[class=nx]").select("a[href]").attr("abs:href");
+
+                    doc = Jsoup.connect(newUrl).get();
+
+                    Elements newSelected = doc.select("tr[class=_selected]").select("th[colspan=7]"); //_selected 클래스는 오늘 경기를 가리켜준다.
+                    Elements newDates = doc.select("th[colspan=7]");
+                    Elements newLeftTeams = doc.select("td[class=l_team]");
+                    Elements newRightTeams = doc.select("td[class=r_team]");
+                    Elements newScores = doc.select("td[class=score]");
+                    Elements newStadiums = doc.select("span[class=stadium_ellipsis]");
+                    Elements newTimes = doc.select("span[class=bg_none]");
+
+                    secondGameStr[0] = newDates.get(0).text();
+                    secondGameStr[1] = newLeftTeams.get(0).text() + " " + newScores.get(0).text() + " " + newRightTeams.get(0).text();
+                    secondGameStr[2] = newStadiums.get(0).text();
+                    secondGameStr[3] = newTimes.get(0).text();
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            TextView tv0 = (TextView)getView().findViewById(R.id.first_match_date);
+            tv0.setText(firstGameStr[0]);
+
+            TextView tv1 = (TextView)getView().findViewById(R.id.first_match_team);
+            tv1.setText(firstGameStr[1]);
+
+            TextView tv2 = (TextView)getView().findViewById(R.id.first_match_stadium);
+            tv2.setText(firstGameStr[2]);
+            if(firstGameStr[2].equals("Tottenham Hotspur Stadium")) tv2.setTextColor(Color.parseColor("#202060"));
+
+            TextView tv3 = (TextView)getView().findViewById(R.id.first_match_time);
+            tv3.setText(firstGameStr[3]);
+
+            TextView tv4 = (TextView)getView().findViewById(R.id.second_match_date);
+            tv4.setText(secondGameStr[0]);
+
+            TextView tv5 = (TextView)getView().findViewById(R.id.second_match_team);
+            tv5.setText(secondGameStr[1]);
+
+            TextView tv6 = (TextView)getView().findViewById(R.id.second_match_stadium);
+            tv6.setText(secondGameStr[2]);
+            if(secondGameStr[2].equals("Tottenham Hotspur Stadium")) tv6.setTextColor(Color.parseColor("#202060"));
+
+            TextView tv7 = (TextView)getView().findViewById(R.id.second_match_time);
+            tv7.setText(secondGameStr[3]);
+
+            progressDialog.dismiss();
+        }
+    }
+}
+
---
+++ TabbedActivity2/app/src/main/java/com/tottenham/tabbedactivity2/RankFragment.java
@@ -0,0 +1,118 @@
+package com.tottenham.tabbedactivity2;
+
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RankFragment extends Fragment {
+
+
+    public RankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_rank, container, false);
+        new EPLRanking().execute();
+        return rootView;
+    }
+
+    private class EPLRanking extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog progressDialog;
+        private String[][] rankInformation = {{"","","","",""},{"","","","",""},{"","","","",""}, {"","","","",""},{"","","","",""},{"","","","",""},
+                {"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},
+                {"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""},{"","","","",""}};//20 X 5 의 2차원 배열을 이용하면 편할 것이다.
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.setMessage("경기 일정을 가져오고 있습니다."); //리그 순위를 가져오는 메시지가 경기 일정을 가져오는 메시지를 덮어써서 문제가 됨, 따라서 하나로 통일함
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Document doc = Jsoup.connect("https://search.naver.com/search.naver?sm=top_hty" +
+                        "&fbm=1&ie=utf8&query=epl+%EC%88%9C%EC%9C%84").get();
+
+                Elements topTeams = doc.select("td[class=team tp wdt]");
+                Elements nomalTeams = doc.select("td[class=team wdt]");
+                Elements informations = doc.select("td[class=tp]");
+
+                for(int i = 0; i < 20; i++) rankInformation[i][0] = Integer.toString(i+1); // 순위를 계산하는 과정
+
+                String[] topTeam = topTeams.text().split(" "); // 이름을 가져오는 과정 (1위 팀)
+                rankInformation[0][1] = topTeam[0];
+
+                String[] nomalTeam = nomalTeams.text().split(" "); // 이름을 가져오는 과정 (나머지 팀)
+                for(int i = 0; i < 19; i++) {
+                    rankInformation[i+1][1] = nomalTeam[i];
+                }
+
+                String[] information = informations.text().split(" ");
+                for(int i = 0; i < 20; i++) {
+                    rankInformation[i][2] = information[8 * i]; //경기를 가져오는 식
+                    rankInformation[i][3] = information[8 * i + 1]; //승점을 가져오는 식
+                    rankInformation[i][4] = information[8 * i + 7]; //득실차를 가져오는 식
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            int[][] idArray = {{R.id.rank1,R.id.team1,R.id.match1,R.id.point1,R.id.goal_difference1},{R.id.rank2,R.id.team2,R.id.match2,R.id.point2,R.id.goal_difference2},
+                    {R.id.rank3,R.id.team3,R.id.match3,R.id.point3,R.id.goal_difference3},{R.id.rank4,R.id.team4,R.id.match4,R.id.point4,R.id.goal_difference4},
+                    {R.id.rank5,R.id.team5,R.id.match5,R.id.point5,R.id.goal_difference5},{R.id.rank6,R.id.team6,R.id.match6,R.id.point6,R.id.goal_difference6},
+                    {R.id.rank7,R.id.team7,R.id.match7,R.id.point7,R.id.goal_difference7},{R.id.rank8,R.id.team8,R.id.match8,R.id.point8,R.id.goal_difference8},
+                    {R.id.rank9,R.id.team9,R.id.match9,R.id.point9,R.id.goal_difference9},{R.id.rank10,R.id.team10,R.id.match10,R.id.point10,R.id.goal_difference10},
+                    {R.id.rank11,R.id.team11,R.id.match11,R.id.point11,R.id.goal_difference11},
+                    {R.id.rank12,R.id.team12,R.id.match12,R.id.point12,R.id.goal_difference12},{R.id.rank13,R.id.team13,R.id.match13,R.id.point13,R.id.goal_difference13},
+                    {R.id.rank14,R.id.team14,R.id.match14,R.id.point14,R.id.goal_difference14},{R.id.rank15,R.id.team15,R.id.match15,R.id.point15,R.id.goal_difference15},
+                    {R.id.rank16,R.id.team16,R.id.match16,R.id.point16,R.id.goal_difference16},{R.id.rank17,R.id.team17,R.id.match17,R.id.point17,R.id.goal_difference17},
+                    {R.id.rank18,R.id.team18,R.id.match18,R.id.point18,R.id.goal_difference18},{R.id.rank19,R.id.team19,R.id.match19,R.id.point19,R.id.goal_difference19},
+                    {R.id.rank20,R.id.team20,R.id.match20,R.id.point20,R.id.goal_difference20}}; //20 X 5 배열로 선언하여 바로 사상하여 사용하도록 한다.
+
+            TextView[][] textViewArray = new TextView[20][5];
+
+            for(int i = 0; i < 20; i++) {
+                for(int j = 0; j < 5; j++) {
+                    textViewArray[i][j] = (TextView)getView().findViewById(idArray[i][j]);
+                    textViewArray[i][j].setText(rankInformation[i][j]);
+                }
+            }
+
+            progressDialog.dismiss();
+        }
+    }
+
+}
